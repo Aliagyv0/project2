0b94039d2ba6672603f885e0b06bc5d7e3c0cfb1
unknown <aliaea@code.edu.az>
2024-01-21 07:46:42 -0800

    first commit

---
+++ week2app2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Week2App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+
+            Console.WriteLine("Which day of the week is it? (As number)");
+             day = int.Parse(Console.ReadLine());
+
+            if (day == 1)
+            {
+                Console.WriteLine("It is the only day that i am free.");
+            }
+            else if (day == 2)
+            {
+                Console.WriteLine("I have online class.");
+            }
+            if (day == 3)
+            {
+                Console.WriteLine("I have Geometry");
+            }
+            else if (day == 4)
+            {
+                Console.WriteLine("I have online class.");
+            }
+            if (day == 5)
+            {
+                Console.WriteLine("It is the only day that i am free.");
+            }
+            else if (day == 6)
+            {
+                Console.WriteLine("I have online class.");
+            }
+            if (day == 7)
+            {
+                Console.WriteLine("It is the only day that i am free.");
+            }
+        }
+         
